4442dd3981cc690aea777987938147176807cfed
Nick Mac <nicsmeyer@gmail.com>
2024-08-29 18:10:26 +0200

    Added crossplatform builds Added DB json

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PDFExporter from './components/PDFExporter';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      { <PDFExporter /> }
       <StatusBar style="auto" />
     </View>
   );
